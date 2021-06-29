0f552412fbe63390e945b2d8b4e19737629a8764
yoshihiro.kinoshita <yoshihiro.kinoshita@scalar-labs.com>
2021-06-29 02:23:44 +0000

    Update loader.sh

--- server/loader.sh
+++ server/loader.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar $SCHEMATOOL/scalar-schema-standalone-2.4.1.jar --cassandra -h docker -u cassandra -p cassandra -f schema/demo-user.json -R 1
+java -jar $SCHEMATOOL/scalar-schema-standalone-2.4.1.jar --cassandra -h localhost -u cassandra -p cassandra -f schema/demo-user.json -R 1
